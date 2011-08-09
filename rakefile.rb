9d7277650856fcfe49a2568a211d2dd8616f67f4
Dan Lash <danlash@gmail.com>
2011-08-09 11:56:23 -0400

    rake tasks for launching solution and publishing to core

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,10 @@
-
 require 'albacore'
+
+@build = {
+	:binaries => "arq/bin",
+	:config => "debug",
+	:core_integration => "../core/lib/arq"
+}
 
 task :default => :compile
 
@@ -12,4 +17,21 @@ msbuild :compile do |msb|
   msb.verbosity = "minimal"
   msb.targets :Rebuild
   msb.solution = 'arq.sln'
+end
+
+desc "Launch VisualStudio"
+task :sln do
+  Thread.new do
+    solution = 'arq.sln'
+    system %{start "" "#{solution}"}
+  end
+end
+
+desc "Copies the binaries to Core"
+task :publish, :path do |t, args|
+  args.with_defaults(:path => @build[:core_integration])
+  destination = args.path
+
+  mkdir_p destination
+  cp_r File.join(@build[:binaries], @build[:config], '.'), destination
 end
